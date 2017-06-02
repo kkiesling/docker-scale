021daa5a10fbf95672ffe76dd854427781d2f8e2
Kalin Kiesling <krkiesling@gmail.com>
2017-06-02 13:23:59 -0500

    fixed incorrect path to mpi

--- install-files/qt.cmake
+++ install-files/qt.cmake
@@ -10,7 +10,7 @@ SET(LAPACK_LIBRARY_NAMES lapack CACHE STRING "")
 SET(BLAS_LIBRARY_NAMES blas CACHE STRING "")
 
 SET(MPI_EXEC "/home/install/openmpi/bin/mpiexec" CACHE FILEPATH "")
-SET(CMAKE_CXX_COMPILER "/home/openmpi/bin/mpicxx" CACHE FILEPATH "" )
+SET(CMAKE_CXX_COMPILER "/home/install/openmpi/bin/mpicxx" CACHE FILEPATH "" )
 SET(CMAKE_C_COMPILER "/home/install/openmpi/bin/mpicc" CACHE FILEPATH "" )
 SET(CMAKE_Fortran_COMPILER "/home/install/openmpi/bin/mpif90" CACHE FILEPATH "")
 
